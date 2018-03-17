d9995f4c5b6bc2edf24a031c91357322c30034d2
Ravi Savajiyani <rsavaj@gmail.com>
2018-03-16 22:05:47 -0700

    update app.js

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,5 +1,4 @@
 $(document).ready(function () {
-    $(".button-collapse").sideNav();
     $('#scrape-button').on('click', () => {
         $.getJSON('/scrape', () => console.log('scraper pressed'));
     });
