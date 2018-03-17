046651f23350ed146d1837d893c63f11dbeb86dc
Ravi <rsavaj@gmail.com>
2018-03-16 20:49:26 -0700

    update models

--- models/Note.js
+++ models/Note.js
@@ -1,20 +1,19 @@
-var mongoose = require("mongoose");
-
-var Schema = mongoose.Schema;
+const mongoose = require("mongoose");
+// Create a schema class
+const Schema = mongoose.Schema;
 
 // Create the Note schema
-var NoteSchema = new Schema({
-    body: {
+const NoteSchema = new Schema({
+    title: {
         type: String
     },
-    article: {
-        type: Schema.Types.ObjectId,
-        ref: "Article"
+    body: {
+        type: String
     }
 });
 
 // Create the Note model with the NoteSchema
-var Note = mongoose.model("Note", NoteSchema);
+const Note = mongoose.model("Note", NoteSchema);
 
 // Export the Note model
 module.exports = Note;
